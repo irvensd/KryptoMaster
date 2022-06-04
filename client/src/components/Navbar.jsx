0897f03e3507a352cbfc70f144771aee36b9be35
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-06-03 22:10:02 -0400

    Front end

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from "../../images/logo.png";
+
+const NavbarItem = ({ title, classProps }) => {
+  return (
+    <li className={`mx-4 cursor-pointer ${classProps}`}>
+      {title}
+    </li>
+  )
+}
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex md:justify-center justify-between items-center =-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 curson-pointer" />
+      </div>
+      <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+      {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index ) => (
+
+      ))}
+      </ul>
+    </nav>
   )
 }
 
