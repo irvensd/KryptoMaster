ec67c2c7c28b672f7e6bef37bff4a30937137346
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-07-03 15:50:20 -0400

    Smartcontract

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,13 @@
+// https://eth-rinkeby.alchemyapi.io/v2/DQeZ6tgyb5KUQ0unATej9yV_fgj9cUQh
+
+require('@nomiclabs/hardhat-waffle')
+
+moduel.exports - {
+  solidity: '0.8.8', 
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/DQeZ6tgyb5KUQ0unATej9yV_fgj9cUQh',
+      accounts: [ '19446d68793406a49fc5a2654494fe519308588857ae9ec64e059db4e0993789' ]
+    }
+  }
+}
